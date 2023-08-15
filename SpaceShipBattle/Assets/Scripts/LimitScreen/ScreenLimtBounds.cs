a8db3e3ace7cd02b1f485b70f5ab43cf48b365e6
Polaldo <darth502002@gmail.com>
2023-08-15 20:33:47 +0200

    WIP ScreenBoundaries

---
+++ SpaceShipBattle/Assets/Scripts/LimitScreen/ScreenLimtBounds.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScreenLimtBounds : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 pos = Camera.main.WorldToViewportPoint(transform.position);
+        pos.x = Mathf.Clamp01(pos.x);
+        pos.y = Mathf.Clamp01(pos.y);
+        transform.position = Camera.main.ViewportToWorldPoint(pos);
+    }
+}
