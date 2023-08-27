27975c962f228707e9f594b1d53266761b805048
Polaldo <darth502002@gmail.com>
2023-08-27 20:30:12 +0200

    engine animation

--- SpaceShipBattle/Assets/Scripts/SpaceShipBehaviour/PlayerShip/PlayerShipBehaviour.cs
+++ SpaceShipBattle/Assets/Scripts/SpaceShipBehaviour/PlayerShip/PlayerShipBehaviour.cs
@@ -1,15 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Numerics;
 using UnityEngine;
 using UnityEngine.UIElements;
+using Vector2 = UnityEngine.Vector2;
 
 public class PlayerShipBehaviour : SpaceShipBehaviour
 {
     private PlayerShipData _playerShipData;
     [SerializeField] GameObject _baseShip;
     [SerializeField] GameObject _engineShip;
-    [SerializeField] GameObject _primaryWeaponShip;
+    [SerializeField] GameObject _primaryWeaponShip;  
+    [SerializeField] Animator _engineAnimation;
     [SerializeField] float speed;
+    float targetSpeed;
+    Vector2 moveDir;
 
     protected override void Start()
     {
@@ -28,9 +33,19 @@ public class PlayerShipBehaviour : SpaceShipBehaviour
     private void FixedUpdate()
     {
         MoveShip();
+        SetValuesAnimation();
     }
+
     private void MoveShip()
     {
-        rb.MovePosition(rb.position + InputManager.Instance.GetInputMovePlayer().normalized * speed * Time.fixedDeltaTime);
+        
+        moveDir = InputManager.Instance.GetInputMovePlayer().normalized;   
+        rb.MovePosition(rb.position + moveDir * speed * Time.fixedDeltaTime);
     }
+
+    private void SetValuesAnimation()
+    {
+        _engineAnimation.SetFloat("Moving", moveDir.magnitude);
+    }
+    
 }
