33a2bc61d6e594dd3646258eee40c0745dc0684f
Polaldo <darth502002@gmail.com>
2024-02-27 20:20:46 +0100

    fix bug with the collisions of bullets and implemented that the bullet instantiate on the cannons of the ship

--- SpaceShipBattle/Assets/Scripts/SpaceShipBehaviour/Weapons/PrimaryWeapon/PrimaryWeaponBehaviour.cs
+++ SpaceShipBattle/Assets/Scripts/SpaceShipBehaviour/Weapons/PrimaryWeapon/PrimaryWeaponBehaviour.cs
@@ -1,14 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
+using Unity.Burst.Intrinsics;
 using UnityEngine;
 using static UnityEngine.GraphicsBuffer;
 
 public class PrimaryWeaponBehaviour : WeaponBehaviour<PrimaryWeaponData>
 {
     public Transform[] spawnsBullets;
+    private int resetIndex = 1; //because getComponentsInChildren also gets the parent tranform
+    private int index;
+
+
     private void Start()
     {
         this.GetComponent<SpriteRenderer>().sprite = weaponData.primaryWeaponSprite;
+        spawnsBullets = gameObject.GetComponentsInChildren<Transform>();
+        index = resetIndex;
     }
     private void Update()
     {
@@ -27,15 +35,21 @@ public class PrimaryWeaponBehaviour : WeaponBehaviour<PrimaryWeaponData>
 
     public override void Shoot()
     {
-            GameObject tempBullet = ObjectPool.SharedInstance.GetPooledObject();
-            if (tempBullet != null)
-            {
-                tempBullet.transform.position = spawnsBullets[0].transform.position;
-                tempBullet.transform.rotation = spawnsBullets[0].transform.rotation;
-                tempBullet.SetActive(true);
-            }
+        GameObject tempBullet = ObjectPool.SharedInstance.GetPooledObject();
+        if (tempBullet != null)
+        {
+            SetTransformTempBullet(tempBullet.transform);
+            tempBullet.SetActive(true);
 
-            SetBulletData(tempBullet);    
+            SetBulletData(tempBullet);
+        }      
+    }
+
+    public void SetTransformTempBullet(Transform bulletTransform)
+    {
+        bulletTransform.position = spawnsBullets[index].position;
+        index = index >= spawnsBullets.Length -1 ? resetIndex : ++index;
+        Debug.Log(index);
     }
 
     public void SetBulletData(GameObject tempBullet)
