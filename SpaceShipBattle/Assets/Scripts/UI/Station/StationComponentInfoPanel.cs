6ebcecaf096731e647732d066b5d17344840e020
Polaldo <darth502002@gmail.com>
2025-07-22 22:12:36 +0200

    create station panel and component info panel

---
+++ SpaceShipBattle/Assets/Scripts/UI/Station/StationComponentInfoPanel.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class StationComponentInfoPanel : MonoBehaviour
+{
+    [SerializeField] private GameObject componentInfoPanel;
+
+    [Header("Info components UI")]
+    [SerializeField] private Image sprite;
+    [SerializeField] private TextMeshProUGUI description;
+    [SerializeField] private TextMeshProUGUI health;
+    [SerializeField] private TextMeshProUGUI attack;
+    [SerializeField] private TextMeshProUGUI defense;
+    [SerializeField] private TextMeshProUGUI speed;
+
+    public void OpenComponentInfoPanel(ComponentShipData componentShipData)
+    {
+        SetComponentInfo(componentShipData);
+        componentInfoPanel.gameObject.SetActive(true);
+    }
+
+    private void SetComponentInfo(ComponentShipData componentShipData)
+    {
+        sprite.sprite = componentShipData.sprite;
+        health.text = componentShipData.health.ToString();
+        attack.text = componentShipData.damage.ToString();
+        defense.text = componentShipData.defense.ToString();
+        speed.text = componentShipData.speed.ToString();
+    }
+
+    public void CloseComponent()
+    {
+        componentInfoPanel.gameObject.SetActive(false);
+    }
+}
--- SpaceShipBattle/Assets/Scripts/UI/Station/StationUIController.cs
+++ SpaceShipBattle/Assets/Scripts/UI/Station/StationUIController.cs
@@ -1,9 +1,40 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 public class StationUIController : MonoBehaviour
 {
-    public void SpaceShipPannel()
-    {
+    private PlayerShipData playerShipData;
 
+    [Header("Components Ship UI")]
+    [SerializeField] private GameObject baseShip;
+    [SerializeField] private GameObject weapon;
+    [SerializeField] private GameObject abilityWeapon;
+    [SerializeField] private GameObject engine;
+
+    [Header("Info Panel")]
+    [SerializeField] private StationComponentInfoPanel stationInfoPanel;
+
+    private void Start()
+    {
+        playerShipData = PlayerManager.Instance.shipData;
+        SetSpritesUI();
+        SetListenersToButtons();
+    }
+
+    private void SetSpritesUI()
+    {
+        //Hardcoded bc we want the child component and not the component of itself
+        baseShip.GetComponentsInChildren<Image>()[1].sprite = playerShipData.baseShipData.sprite;
+        weapon.GetComponentsInChildren<Image>()[1].sprite = playerShipData.primaryWeaponData.sprite;
+        abilityWeapon.GetComponentsInChildren<Image>()[1].sprite = playerShipData.abilityWeaponData.sprite;
+        engine.GetComponentsInChildren<Image>()[1].sprite = playerShipData.engineData.sprite;
+    }
+
+    private void SetListenersToButtons()
+    {
+        baseShip.GetComponent<Button>().onClick.AddListener(() => stationInfoPanel.OpenComponentInfoPanel(playerShipData.baseShipData));
+        weapon.GetComponent<Button>().onClick.AddListener(() => stationInfoPanel.OpenComponentInfoPanel(playerShipData.primaryWeaponData));
+        abilityWeapon.GetComponent<Button>().onClick.AddListener(() => stationInfoPanel.OpenComponentInfoPanel(playerShipData.abilityWeaponData));
+        engine.GetComponent<Button>().onClick.AddListener(() => stationInfoPanel.OpenComponentInfoPanel(playerShipData.engineData));
     }
 }
